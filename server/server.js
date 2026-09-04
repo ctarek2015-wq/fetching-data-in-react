9bbe34bee4dbf4ffd972891cd8ea3b84b80e7e51
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-04 19:42:07 +0300

    Add initial server setup with Express and weather API integration
    - Created package.json for server dependencies including express, cors, and dotenv.
    - Implemented server.js to set up an Express server.
    - Added a route to fetch weather data from the Weather API based on city parameter.
    - Configured server to listen on port 3000.

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+// Define a BASE_URL that includes the API_KEY:
+const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}`;
+
+app.get("/weather/:city", async (req, res) => {
+  const { city } = req.params; // Destructure city from req.params
+  const queryString = `&q=${city}`; // Create a queryString using city
+  const apiRes = await fetch(BASE_URL + queryString); // Make the fetch request
+  const data = await apiRes.json(); // Parse the response
+  res.json(data); // Respond to the request with the data
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000...");
+});
