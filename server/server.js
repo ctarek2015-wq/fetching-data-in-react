755ffca1150763be131a99509a01020635618e64
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-04 19:58:30 +0300

    Update server and weather service configuration; add morgan and nodemon, and refactor API URL handling

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-
+app.use(cors());
+const morgan = require("morgan");
+app.use(morgan("dev"));
 // Define a BASE_URL that includes the API_KEY:
 const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}`;
 
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,12 +1,9 @@
-const API_KEY = "d8256c892f0343e2a1665718260309";
-const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+const BASE_URL = `http://localhost:3000/weather/`;
 
 const show = async (city) => {
-  const queryStringURL = `${BASE_URL}&q=${city}`;
   try {
-    const jsonRes = await fetch(queryStringURL);
-    const data = await jsonRes.json();
-
+    const res = await fetch(BASE_URL + city);
+    const data = await res.json();
     return data;
   } catch (error) {
     console.log(error.message);
